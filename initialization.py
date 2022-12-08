03dc16d35d1fe853bba2c7a27060042fe9ceb799
Dirk Djently <120066489+Dirk-Djently@users.noreply.github.com>
2022-12-08 00:37:35 -0600

    Update initialization.py

--- initialization.py
+++ initialization.py
@@ -6,7 +6,7 @@ reqs = subprocess.check_output([sys.executable, '-m', 'pip',
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
 if 'pynput' in installed_packages and 'PyAutoGUI' in installed_packages and 'barnum' in installed_packages and 'keyboard' in installed_packages:
-    print('required packages already in place \n')
+    print('required packages already in place\n')
 
 else:
     print('downloading necessary packages')
@@ -24,6 +24,6 @@ else:
     installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
     if 'pynput' in installed_packages and 'PyAutoGUI' in installed_packages and 'barnum' in installed_packages:
-        print("installation successful")
+        print("installation successful\n")
     else:
         print('error during module installation. exit program and try again')
